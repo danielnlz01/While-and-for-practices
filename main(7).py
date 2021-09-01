761dacc90c1ec6a7a921847ac94d519011acb031
danielnlz01 <38385574+danielnlz01@users.noreply.github.com>
2021-08-31 20:10:42 -0500

    Add files via upload

---
+++ main(7).py
@@ -0,0 +1,29 @@
+for i in range(5,1,-1):
+  print(i)
+  
+contador=0
+while contador<5:
+  print(contador)
+  contador+=1
+  
+total=0
+num=0
+
+for num in range(101):
+  total=total+num
+print(total)
+
+while (num<101):
+  total=total+num
+  num+=1
+print(total)
+
+for i in range(5,10): #para tener rango personalizado
+  print(i)
+  
+for i in range(2,20,2): #para incrementar cada 2
+  print(i)
+
+for i in range(10,0,-1): #para contar en orden regresivo, siempre suma 1 por default
+  print(i)
+  
